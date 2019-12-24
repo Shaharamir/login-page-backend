4cf8b827ee7f1a1bc333a5a411e04a317cd152cd
Shahar <shahramir3@gmail.com>
2019-12-24 20:28:13 +0200

    fixed bug

--- index.ts
+++ index.ts
@@ -49,45 +49,106 @@ interface IGame {
   }
 }
 
-// row: number, column: number, moveToColumn: number, moveToRow: number )
-const moveChecker = (JSONStringify: string, current: {row: number, col: number}, target: {row: number, col: number}) => {
-  const initChecker = {
-      isChecker: false,
-      checkerColor: undefined,
+const gameInit: IGame[][] = Array.from({ length: 8 }, (v, rowIndex) => Array.from({ length: 8 }, (v, columnIndex) => {
+  const checkerColor = rowIndex <= 2 ? 'white' : (rowIndex >= 5 ? 'black' : undefined);
+  const evenRow = columnIndex % 2 === 0;
+  const rowsAllowed = rowIndex <= 2 && rowIndex >= 5;
+  const isChecker = (checkerColor !== undefined) && (rowIndex % 2 === 0 ? evenRow : !evenRow);
+  return {
+      square: {
+          row: rowIndex,
+          column: columnIndex,
+          isChecker: isChecker,
+          checkerColor: isChecker ? checkerColor : undefined,
+          squareColor: columnIndex % 2 === 0 ? rowIndex % 2 === 0 ? 'black' : 'white' : rowIndex % 2 === 0 ? 'white' : 'black',
+          shouldHighlight: false
+      }
   }
+}))
 
-  const setChecker = (color: 'white' | 'black') => {
-    return {
-        isChecker: true,
-        checkerColor: color
-    }
+const moveChecker = (game: IGame[][], current: {row: number, col: number}, target: {row: number, col: number}) => {
+  //if eated
+  if(Math.abs(current.row - target.row) === 2) {
+      const eatedRow = (current.row+target.row)/2
+      const eatedCol = (current.col+target.col)/2
+      game[eatedRow][eatedCol].square.isChecker = false;
+      game[eatedRow][eatedCol].square.checkerColor = undefined;
   }
-  
-  const game: IGame[][] = JSON.parse(JSONStringify)
-  game.map((row, rowIndex) => row.map((col, colIndex) => game[rowIndex][colIndex].square.shouldHighlight = false));
   const currentChecker = game[current.row][current.col].square;
-  const moveToChecker = game[target.row][target.col].square
-  game[current.row][current.col].square = {...currentChecker, ...initChecker}
-  game[target.row][target.col].square = {...moveToChecker, ...setChecker(currentChecker.checkerColor)}
+  game[current.row][current.col].square.isChecker = false;
+  game[target.row][target.col].square.isChecker = true;
+  game[target.row][target.col].square.checkerColor = currentChecker.checkerColor;
+  game[current.row][current.col].square.checkerColor = undefined;
   return game;
+}
+
+
+const checkIfTwoPlayersConnected = () => {
+  return Boolean(io.sockets.adapter.rooms['room'].length === 2);
+}
+
+let roomsGames = {};
+
+const toggleTurn = (currentTurn: number) => {
+  if (currentTurn === 0) {
+    return 1;
+  }
+  return 0;
 }
 
 io.on('connection', (socket: Socket) => {
     const user: IDataBaseUser = socket.handshake.query.user;
-    socket.join('fuck');
-    // console.log(io.sockets.connected)
+    socket.join('room');
+    if(checkIfTwoPlayersConnected()) {
+      roomsGames = {
+        'room': {
+          'board': gameInit,
+          'turn': 0
+        }
+      };
+      io.to('room').emit('gameStart', Object.keys(io.sockets.adapter.rooms['room'].sockets)[0]);
+    }
     console.log('User connected')
     socket.on('disconnect', () => {
       console.log('user disconnected')
     })
-    socket.on('move', ({turn, current, target, board}) => {
-      // console.log(`move start: \n ${turn}, ${board}`)
-      currentBoard = board;
-      currentTurn = turn;
-      const upadatedBoard = moveChecker(board, current, target);
-      currentBoard = upadatedBoard;
-      socket.emit('moveEnd', ({turn: currentTurn, board: currentBoard}))
+    socket.on('move', ({ current, target }) => {
+      const currentPlayer = roomsGames['room'].turn;
+      const shouldUseMove = Object.keys(io.sockets.adapter.rooms['room'].sockets)[currentPlayer] === socket.id;
+      if (shouldUseMove) {
+        console.log('-------OLD-------');
+        printBoard(roomsGames['room'].board);
+        if (currentPlayer === 0) {
+          current.row = 7 - current.row;
+          current.col = 7 - current.col;
+          target.row = 7 - target.row;
+          target.col = 7 - target.col;
+          roomsGames['room'].board = moveChecker(roomsGames['room'].board, current, target);
+          roomsGames['room'].turn = toggleTurn(roomsGames['room'].turn);
+        } else {
+          roomsGames['room'].board = moveChecker(roomsGames['room'].board, current, target);
+          roomsGames['room'].turn = toggleTurn(roomsGames['room'].turn);
+          current.row = 7 - current.row;
+          current.col = 7 - current.col;
+          target.row = 7 - target.row;
+          target.col = 7 - target.col;
+        }
+        socket.to('room').emit('moveEnd', {current, target});
+        console.log('-------NEW-------');
+        printBoard(roomsGames['room'].board);
+        // console.log(JSON.stringify(roomsGames['room'].board));
+      }
     })
   })
+
+  const printBoard = (board: IGame[][]) => {
+    board.map((row, rowIndex) => row.map((col, colIndex) => {
+      const color = board[rowIndex][colIndex].square.checkerColor
+      process.stdout.write(color === 'black' ? ' 🍦 ' : color === 'white' ? ' 🍪 ' : ' 🍫 ');
+      if(colIndex === 7) {
+        console.log('');
+      }
+    }))
+  }
 
 module.exports = server;
